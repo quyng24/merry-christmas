cebcf4be77ca8eb27897cb171d0505b09d868584
Nguyen Quy <nquy50771@gmail.com>
2024-11-19 10:37:37 +0700

    upload source code 2

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function generateTree() {
     for (let j = 0; j < 2 * i + 1; j++) {
       // Tạo ký tự 0 hoặc 1
       const isRed = Math.random() < 0.1; // 10% đổi màu đỏ
-      const color = isRed ? "red" : "green";
+      const color = isRed ? "#f82727" : "#3ff65d";
       const digit = Math.random() < 0.5 ? "0" : "1";
 
       rowText += `<span style="color: ${color};">${digit}</span>`;
@@ -37,22 +37,4 @@ function generateTree() {
 // Thay đổi liên tục
 setInterval(generateTree, 500);
 
-var thep = document.querySelector('.name');
-var originContent = thep.innerHTML;
-var index = 0;
-var dk = true;
-var time = 200;
-setInterval(function() {
-    if(dk) {
-        index++;
-        if(index > originContent.length) {
-            dk = false;
-        }
-    } else{
-        index--;
-        if(index < 0) {
-            dk = true;
-        }
-    }
-    thep.innerHTML = originContent.substring(0, index);
-}, time)
+
